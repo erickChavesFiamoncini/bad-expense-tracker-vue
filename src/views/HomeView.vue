0a589b1d71f27f35b595795d9e84144e7043dfef
erickChavesFiamoncini <erickechaves@gmail.com>
2026-03-06 14:45:10 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import AppHeader from '@/components/layout/AppHeader.vue';
+</script>
+
+<template>
+    <AppHeader title="Controle de Gastos Rapido" />
+</template>
